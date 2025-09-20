496b75801f31c40ab57508691ec396394cd1ca4c
axmed-1 <akmed.mikailov@bk.ru>
2025-09-20 11:14:18 +0300

    change file

--- src/ifElse.kt
+++ src/ifElse.kt
@@ -1,5 +1,37 @@
-fun main(){
+    fun main(){
+        // задача1
+      // println("Введите первое число")
+       // val number1 = readln().toInt()
+        //println("Введите втрое число")
+        //val number2 = readln().toInt()
+        //if (number1 > number2){
+           // println("Первое число больше")
 
-    val number  = 10;
-    println(number)
-}
+        // } else {
+           // println("Второе число больше")
+
+        // }
+
+        // val max = if (number1 > number2) number1 else number2
+        // println("Максимальное число ${max}")
+        // val age = readln().toInt()
+        //val status = if (age >= 18) "Совершенолетний" else "Несовершнолетний"
+    //задача2
+    //println("Введите урон")
+    //    val num1 = true
+    //    val num2 = 100.0
+    //    println("Урон:$ if(num1) num2 / 2 else num2 " )
+
+    //задача3
+     //   println("Введите ваш уровень")
+       // val level = readln().toInt()
+        //println("У вас есть вип статус?(true/false)")
+        //val hasVIP = readln().toBoolean()
+        //if ((level >= 30 && hasVIP) || level >= 50){
+          //  println("Доступ разрешен")
+        //} else {
+          //  println("Доступ запрешен")
+        //}
+        // шаг5 Диапазоны
+
+    }
---
+++ src/range.kt
@@ -0,0 +1,16 @@
+fun main(){
+   // val numbers = 1 < 50 < 100
+    //val symbol = 'a' <  'z'
+
+
+   // println("Введите ваш уровень")
+   // val level = readln().toInt()
+    //if (level in 1 < 0 < 50){
+      //  print("Уровень $level соответствует требованиям! Вы можете зайти ")
+    //} else {
+    //    println("Высокий уровень! Вход запрещен!")
+    //}
+
+
+
+    }
