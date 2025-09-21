05cb4b58a0fd3e54569102516db56f064efcf188
axmed-1 <akmed.mikailov@bk.ru>
2025-09-21 13:19:54 +0300

    change file

--- src/When.kt
+++ src/When.kt
@@ -1,4 +1,4 @@
-fun  main(){
+//fun  main(){
  //   println("Введите порядковый номер месяца (1-12):")
    // val index = readln().toInt()
     //val month = ""
@@ -20,37 +20,80 @@ fun  main(){
    // println("Заклинание защиты активировано!")
     //println("Заклинание защиты активировано!")
    // println("Замок защищен!")
-    println("Начинаем ритуал защиты ")
-    repeat (times = 5){
-        println("Заклинание от защиты активирована")
-    }
-    println("Замок защищен")
-    println("Начинаем ритуал защиты")
-    var index = 0
-    repeat(times = 5){
-        println("Заклинание защиты активировано (Шаг ${index + 1})")
-        index ++
-        println("Начинаем ритуал защиты")
-        repeat(times = 5){index ->
-            println("Заклинание защиты активировано ((Шаг ${index + 1})\")")
-        }
-        println("Замок защищен")
-        repeat(times = 5){index ->
-            if (index == 2){
-                println("Пропускаем шаг $ {index + 1} из за магического вмешательства")
-                return@repeat
+   // println("Начинаем ритуал защиты ")
+    //repeat (times = 5){
+      //  println("Заклинание от защиты активирована")
+    //}
+   // println("Замок защищен")
+    //println("Начинаем ритуал защиты")
+    //var index = 0
+    //repeat(times = 5){
+      //  println("Заклинание защиты активировано (Шаг ${index + 1})")
+        //index ++
+        //println("Начинаем ритуал защиты")
+        //repeat(times = 5){index ->
+          //  println("Заклинание защиты активировано ((Шаг ${index + 1})\")")
+        //}
+        //println("Замок защищен")
+        //repeat(times = 5){index ->
+          //  if (index == 2){
+            //    println("Пропускаем шаг $ {index + 1} из за магического вмешательства")
+              //  return@repeat
 
-            }
-            println("Заклинание защиты активировано$ {index + 1}")
+            //}
+            //println("Заклинание защиты активировано$ {index + 1}")
+
+   // val daysInMonth = listOf(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+
+    //print("Введите номер месяца (1-12): ")
+    //val input = readln().toInt()
+
+    //if (input in 1..12) {
+      //  val days = daysInMonth[input - 1]
+        //println("В этом месяце $days дней.")
+    //} else {
+      //  println("Неверный номер месяца. Введите число от 1 до 12.")
+    //}
+    //if (input in 1..12) {
+        //val days = daysInMonth[input - 1]
+       // println("В этом месяце $days дней.")
+    //} else {
+      //  println("Некорректный номер месяца.")
+    //}
+    //fun main() {
+      //  val lowerCaseLetter: Char = 'a'
+        //val upperCaseLetter: Char = 'Q'
+        //val number: Char = '1'
+        //val space: Char = ' '
+        //val dollar: Char = '$'
+
+        //val ch = '\u0040'
+      //  println(ch)
+    //}
+    //val ch = 'a'
+   // println(ch.code) // 97
+    //val num = 97
+    //println(num.toChar()) // a
+
+
+    //val ch: Char = readln().first()
+
+    //val ch1 = 'b'
+    //val ch2 = ch1 + 1 // 'c'
+    //val ch3 = ch2 - 2 // 'a'
+    //val ch = 'A'
+    //ch += 10
+    //println(ch) // 'K'
+    //println(++ch) // 'L'
+    //println(++ch) // 'M'
+    //println(--ch) // 'L'
+
+      // }
 
 
 
-        }
-    }
 
 
-
-}
 
 
 
