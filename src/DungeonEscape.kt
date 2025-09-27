22c775705ce50ed57d9d31d6668232df3b5299ae
axmed-1 <akmed.mikailov@bk.ru>
2025-09-27 11:25:50 +0300

    commit install

---
+++ src/DungeonEscape.kt
@@ -0,0 +1,118 @@
+fun main(){
+
+//     var level = 0
+//    println("Добро пожаловать в Подземелье")
+//    while(level < 5){
+//        println("\nВы на уровне $level")
+//        println("Выберите действие: [1] Вперед ! [2] осмотреться ! [0] Сдаться")
+//        val input = readln()
+//        when(input) {
+//            "1" -> {}
+//
+//            level ++
+//                println ("вы переходите на уровень $level")
+//
+//            "2" -> {
+//            println ("Вы осматриваетесь. Тут ничего интересного")
+//                continue
+//            "0" -> {
+//            }
+//
+//
+//
+//            }
+//            else -> {}
+
+//шаг4
+//   var number: Int
+//  do {
+//      print("введите число больше 10:")
+//      number = readln().toInt()
+//  }while (number <= 10)
+//
+//  println("Спасибо! Вы ввели $number ")
+//
+//
+//
+//
+//    print("Введите пароль:")
+//    var password: String? = readln()
+//    while (password != "qwerty" ){
+//        print("введите пароль")
+//        password = readln()
+//
+//
+//
+//
+//    }
+//  println("Доступ рвзрешен")
+//шаг 5
+//    for (i in 1 <- .. <- 5) {
+//        println("Шаг $i")
+//    }
+//
+//
+//
+
+
+//val feuits = listOf("apple", "banana", "cherry" )
+
+//    for (fruit in fruits){
+//
+//        println("фрукт $fruit")
+//
+//
+//
+//    }
+
+
+//    val sentence ="Kotlin is awesome"
+//    val words =sentence.split()
+//    println()
+//    val data ="apple,banana,orange"
+//    val fruits = data.split()
+//    for (fruit in fruits ){
+//        println(fruit)
+//        val messy = "word1,word2;word3|word4"
+//        val parts = messy.split()
+//        println(parts)
+//
+//        val fullName = "иванов иван"
+//        val parts = messy.split()
+//        val lastName = parts[0]
+//        val firtsName = parts[1]
+//        println("Фамилия $lastName Имя $firtsName")
+
+
+//        print("Введите число через пробел")
+//        val input = readLn()
+//          val numbers = input.split()
+//          var sum = 0
+//    for (number in numbers ){
+//
+//
+//        sum+= number.toInt()
+//
+//
+//
+//        println("Сумма чисел $sum")
+//
+//    }
+// шаг 7
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/Loop2.kt
@@ -0,0 +1,65 @@
+fun main(){
+// while("Условие){
+//}
+// val number = 1{
+// while (number <= 5){
+//}
+//}
+//
+//
+//while (number <= 5){
+//    println("Число: $number")
+//}   number++
+// var number = 1
+// while (number <= 5){
+// //    println("Число: $number")
+////}   number++
+//шаг2
+// fun main(){
+//var number = 1
+//while (number <= 10){
+//    println(number)
+//    number ++
+//    }
+//}
+//while (number <= 10){
+//    println(number)
+//    number++
+//    if (number == 5){
+//    }
+//    println("Останавливаемся на $number")
+//    break
+//}
+//}
+
+
+//fun main(){
+//var number = 0
+//while (number < 10){
+//number++
+//if (number == 3){
+//println("Останавливаемся на $number")
+//continue
+//}
+//println(number)
+
+//println("Напишите что нибудь (для выхода введите 'выход'):")
+//while (true) {
+//val input = readln()
+//if (input == "выход"){
+//}
+//
+//println("До свидание")
+//break
+//}
+//println("Вы ввели:$input")
+
+//var letter = 'A'
+//while(letter <= 'Z'){
+//print (letter)
+//letter++
+//}
+//}
+
+//шаг3 побег из подземелья
+}
