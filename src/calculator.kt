9c34f706cfedb4580ca0c5c50785fed99bbe9d3c
axmed-1 <akmed.mikailov@bk.ru>
2025-09-27 12:35:58 +0300

    commit install

---
+++ src/calculator.kt
@@ -0,0 +1,13 @@
+fun main(){
+//    print("Введите первое число,знак операции и второе число через пробел ")
+//    val input = readln().split()
+//    val symbol = input [1]
+//    val firstNumber = input[0].toDouble()
+//    var result = 0.0
+//    when (symbol){
+
+ //шаг 8
+
+
+    }
+
